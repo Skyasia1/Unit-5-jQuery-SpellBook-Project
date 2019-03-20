c281b8448589a45eb649f3d2466deaa09f4b80ac
Skyasia1 <43544251+Skyasia1@users.noreply.github.com>
2019-03-20 10:06:12 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+$(".magicianButton").click(function(){
+$(".magicianPic").slideUp();
+ 
+});
+
+
+
+//Spell #2
+
+$(".drakeButton").click(function(){
+ $(".drakePic").hide();
+});
+
+
+
+//Spell #3
+
+$(".beyonceButton").click(function(){
+$(".beyoncePic").fadeOut();
+ 
+});
+
+
+
